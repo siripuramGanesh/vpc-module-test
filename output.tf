27a670dc8499516b23e5cc74d3a163bf135eb7c5
ganesh siripuram <siripuramganesh4444@gmail.com>
2024-12-18 13:54:01 +0530

    message

--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "vpc_id"{
 output "az_info"{
     value=module.vpc.az_info
 }
+
+output "vpc_default_info"{
+    value=module.vpc.vpc_default_info
+}
+
+output "vpc_route_table_default"{
+    value=module.vpc.route_table_default_info
+}
